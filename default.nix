3f156cba8f36d88721e24c2e9570cd044ddd93c1
John D. Boy <jboy@bius.moe>
2020-07-08 13:11:38 +0200

    Install all language models in notebook environment.

--- default.nix
+++ default.nix
@@ -16,7 +16,23 @@ stdenv.mkDerivation rec {
     SOURCE_DATE_EPOCH=$(date +%s)
     export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
     pip install git+https://github.com/jboynyc/textnets.git@trunk#egg=textnets-stable
-    python -m spacy download en_core_web_sm
+    models=("de_core_news_sm"
+            "el_core_news_sm"
+            "en_core_web_sm"
+            "es_core_news_sm"
+            "fr_core_news_sm"
+            "it_core_news_sm"
+            "ja_core_news_sm"
+            "lt_core_news_sm"
+            "nb_core_news_sm"
+            "nl_core_news_sm"
+            "pl_core_news_sm"
+            "pt_core_news_sm"
+            "ro_core_news_sm"
+            "zh_core_web_sm")
+    for m in "''${models[@]}";
+      do python -m spacy download $m
+    done
     pip install notebook
     python -m ipykernel install --user --name $name --display-name "Python 3.8 (with textnets)"
   '';
