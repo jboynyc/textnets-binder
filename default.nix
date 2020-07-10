df96a0f919121b7738cf9d15e097eb82f310c2f7
John D. Boy <jboy@bius.moe>
2020-07-10 14:12:15 +0200

    pin versions

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   postShellHook = ''
     SOURCE_DATE_EPOCH=$(date +%s)
     export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
-    pip install textnets==0.4.7
+    pip install textnets==0.4.8
     models=("de_core_news_sm"
             "el_core_news_sm"
             "en_core_web_sm"
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
     for m in "''${models[@]}";
       do python -m spacy download $m
     done
-    pip install notebook
+    pip install notebook==6.0.3
     python -m ipykernel install --user --name $name --display-name "Python 3.8 (with textnets)"
   '';
 }
